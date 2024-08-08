6de54b36607fd5a2e17f1b87584f1f4921496c56
B Vishal <bvshal27@gmail.com>
2024-08-08 17:59:48 +0530

    vite react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+
+
+function App() {
+  
+
+  return (
+    <>
+   <Chai/>
+   
+   </>  
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,13 @@
+function Chai()
+{
+   return (
+      <>
+    <h2> Chai is ready</h2>
+<h2>
+    <p> function created  chai ()  and 
+       return (  html tag  and close paranthesis) exp  </p> </h2>
+    <h2> export function name</h2>
+    </>
+   ) 
+}
+export default Chai
